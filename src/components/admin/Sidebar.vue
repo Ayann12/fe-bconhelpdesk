aa4a3f975db281db22aa8fb0f810844cfbc6d1f0
Ayann12 <tantowi818@gmail.com>
2026-09-16 09:28:01 +0700

    setup func logout

--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -1,9 +1,20 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+
+const authStore =useAuthStore()
+const { logout } = authStore 
+
+const handleLogout = async () => {
+    await logout()
+}
+
+</script>
 <template>
     <aside class="w-64 bg-white shadow-lg">
         <div class="p-6 border-b border-gray-100">
             <h1 class="text-2xl font-bold text-blue-600 flex items-center">
                 <i data-feather="activity" class="w-8 h-8 mr-2"></i>
-                TickTrack
+                BC-Helpdesk
             </h1>
         </div>
         <nav class="mt-6">
@@ -19,7 +30,7 @@
                 <i data-feather="tag" class="w-5 h-5 mr-3"></i>
                 Tiket
             </RouterLink>
-            <a href="#"
+            <a @click="handleLogout"
                 class="flex items-center px-6 py-3 text-gray-600 hover:bg-gray-50 hover:border-l-4 hover:border-gray-200 mt-6">
                 <i data-feather="log-out" class="w-5 h-5 mr-3"></i>
                 Logout
